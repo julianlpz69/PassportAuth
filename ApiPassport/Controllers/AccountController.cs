ea73f19d892e1a6a048b3ffc93808ba3f4c9f2e8
OSCARJMG23 <oscarjulianm2112@gmail.com>
2023-10-10 14:46:09 -0500

    docs: :sparkles: ordered the code

--- ApiPassport/Controllers/AccountController.cs
+++ ApiPassport/Controllers/AccountController.cs
@@ -34,11 +34,11 @@ namespace ApiPassport.Controllers
 
 
         public AccountController(IUserService userService, IUnitOfWork unitOfWork, ILoginUser loginUser)
-    {
-        _unitOfWork = unitOfWork;
-        _userService = userService;
-        _login_user = loginUser;
-    }
+        {
+            _unitOfWork = unitOfWork;
+            _userService = userService;
+            _login_user = loginUser;
+        }
 
 
         [Route("google-login")]
@@ -46,7 +46,7 @@ namespace ApiPassport.Controllers
         {
             var properties = new AuthenticationProperties { 
                 RedirectUri = Url.Action("GoogleResponse"), 
-                 Items =
+                Items =
                 {
                     { "prompt", "select_account" } // Esto fuerza la selección de cuenta de Google
                 }
@@ -55,92 +55,65 @@ namespace ApiPassport.Controllers
             return Challenge(properties, GoogleDefaults.AuthenticationScheme);
         }
 
-       [Route("google-response")]
-public async Task<IActionResult> GoogleResponse()
-{
-    var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        [Route("google-response")]
+        public async Task<IActionResult> GoogleResponse()
+        {
+                var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
 
+                var claims = result.Principal.Identities.FirstOrDefault()
+                            .Claims.Select(claim => new
+                            {
+                                claim.Type,
+                                claim.Value
+                            });
 
+                var correoElectronicoClaim = result.Principal?.Claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress")?.Value;
+                var pictureClaim = result.Principal?.Claims.FirstOrDefault(c => c.Type == "urn:google:picture")?.Value;
+                var NombreClaim = result.Principal?.Claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name")?.Value;
 
-    var claims = result.Principal.Identities.FirstOrDefault()
-                .Claims.Select(claim => new
+                var user = await _unitOfWork.Users.GetByUserGmailAsync(correoElectronicoClaim);
+
+                if (user == null)
                 {
-                    claim.Type,
-                    claim.Value
-                });
+                    // Redirigir a la página de inicio para usuarios no registrados
+                        return Redirect("http://127.0.0.1:5500/Pagina%20Reto/Html/Pagina_Registrar.html");    }
+                else
+                {
+                var usuarioLogiado =  new LoginUser{
+                        UserName = NombreClaim,
+                        UserEmail = correoElectronicoClaim,
+                        Picture = pictureClaim,
+                    };
+                    _unitOfWork.LoginUsers.Add(usuarioLogiado);
+                    await _unitOfWork.SaveAsync();
+                    // Redirigir a la página de inicio para usuarios registrados
+                    return Redirect("http://127.0.0.1:5500/Pagina%20Reto/Html/Pagina_Inicio_Admin.html");
+                    
+                }
+            }
+        [Route("google-logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return Redirect("http://127.0.0.1:5500/Pagina%20Reto/Html/Index.html");
+        }
 
-    
+        [HttpGet]
+        [Route("ultimo-user")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<LoginUser>> Get()
+        {
+            var Ciudad = await _unitOfWork.LoginUsers.Ultimo();
+            return Ok(Ciudad);
+        }
 
-
-    var correoElectronicoClaim = result.Principal?.Claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress")?.Value;
-    var pictureClaim = result.Principal?.Claims.FirstOrDefault(c => c.Type == "urn:google:picture")?.Value;
-    var NombreClaim = result.Principal?.Claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name")?.Value;
-
-
-
-
-
-    var user = await _unitOfWork.Users.GetByUserGmailAsync(correoElectronicoClaim);
-
-    if (user == null)
-    {
-        // Redirigir a la página de inicio para usuarios no registrados
-            return Redirect("http://127.0.0.1:5500/Pagina%20Reto/Html/Pagina_Registrar.html");    }
-    else
-    {
-      var usuarioLogiado =  new LoginUser{
-            UserName = NombreClaim,
-            UserEmail = correoElectronicoClaim,
-            Picture = pictureClaim,
-        };
-        _unitOfWork.LoginUsers.Add(usuarioLogiado);
-        await _unitOfWork.SaveAsync();
-
-        // Redirigir a la página de inicio para usuarios registrados
-        return Redirect("http://127.0.0.1:5500/Pagina%20Reto/Html/Pagina_Inicio_Admin.html");
-          
-    }
-    }
-
-
-
-
-
-
-    [Route("google-logout")]
-    public async Task<IActionResult> Logout()
-{
-    
-
-    await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-
-    return Redirect("http://127.0.0.1:5500/Pagina%20Reto/Html/Index.html");
-}
-
-
-
-    [HttpGet]
-    [Route("ultimo-user")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-
-    public async Task<ActionResult<LoginUser>> Get()
-    {
-        var Ciudad = await _unitOfWork.LoginUsers.Ultimo();
-        return Ok(Ciudad);
-
-    }
-
-    
-
-
-
-    [Route("google-register")]
+        [Route("google-register")]
         public IActionResult GoogleRegister()
         {
             var properties = new AuthenticationProperties { 
                 RedirectUri = Url.Action("GoogleResponseDos"), 
-                 Items =
+                Items =
                 {
                     { "prompt", "select_account" } // Esto fuerza la selección de cuenta de Google
                 }
@@ -149,67 +122,45 @@ public async Task<IActionResult> GoogleResponse()
             return Challenge(properties, GoogleDefaults.AuthenticationScheme);
         }
 
-       [Route("google-response-Dos")]
-public async Task<IActionResult> GoogleResponseDos()
-{
-    var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        [Route("google-response-Dos")]
+        public async Task<IActionResult> GoogleResponseDos()
+        {
+            var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
 
+            var claims = result.Principal.Identities.FirstOrDefault()
+                        .Claims.Select(claim => new
+                        {
+                            claim.Type,
+                            claim.Value
+                        });
 
-    var claims = result.Principal.Identities.FirstOrDefault()
-                .Claims.Select(claim => new
-                {
-                    claim.Type,
-                    claim.Value
-                });
+            var correoElectronicoClaim = result.Principal?.Claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress")?.Value;
+            var pictureClaim = result.Principal?.Claims.FirstOrDefault(c => c.Type == "urn:google:picture")?.Value;
+            var NombreClaim = result.Principal?.Claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name")?.Value;
 
-    
+            var user = await _unitOfWork.Users.GetByUserGmailAsync(correoElectronicoClaim);
 
-
-    var correoElectronicoClaim = result.Principal?.Claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress")?.Value;
-    var pictureClaim = result.Principal?.Claims.FirstOrDefault(c => c.Type == "urn:google:picture")?.Value;
-    var NombreClaim = result.Principal?.Claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name")?.Value;
-
-
-
-
-
-    var user = await _unitOfWork.Users.GetByUserGmailAsync(correoElectronicoClaim);
-
-    if (user == null)
-    {
-        // Redirigir a la página de inicio para usuarios no registrados
-             var usuarioLogiado =  new RegisterDto{
-            UserName = NombreClaim,
-            UserEmail = correoElectronicoClaim,
-             UserPassword = "123456",
-        };
-        var usuarioJson = JsonConvert.SerializeObject(usuarioLogiado);
-        var data2 = new StringContent(usuarioJson, System.Text.Encoding.UTF8, "application/json");
-        var httpClient2 = new HttpClient();
-        var response2 = await httpClient2.PostAsync("https://localhost:7051/api/user/register", data2);
-        
-
-        // Redirigir a la página de inicio para usuarios registrados
-        return Redirect("http://127.0.0.1:5500/Pagina%20Reto/Html/Pagina_Login.html");
+            if (user == null)
+            {
+                // Redirigir a la página de inicio para usuarios no registrados
+                    var usuarioLogiado =  new RegisterDto{
+                    UserName = NombreClaim,
+                    UserEmail = correoElectronicoClaim,
+                    UserPassword = "123456",
+                };
+                var usuarioJson = JsonConvert.SerializeObject(usuarioLogiado);
+                var data2 = new StringContent(usuarioJson, System.Text.Encoding.UTF8, "application/json");
+                var httpClient2 = new HttpClient();
+                var response2 = await httpClient2.PostAsync("https://localhost:7051/api/user/register", data2);
+                
+                // Redirigir a la página de inicio para usuarios registrados
+                return Redirect("http://127.0.0.1:5500/Pagina%20Reto/Html/Pagina_Login.html");
+            }
+            else
+            {
+                return Redirect("http://127.0.0.1:5500/Pagina%20Reto/Html/Pagina_Login.html");
+            
+            }
+        }
     }
-    else
-    {
-
-        return Redirect("http://127.0.0.1:5500/Pagina%20Reto/Html/Pagina_Login.html");
-     
-          
-    }
-    }
-
-
-    }
-    
-
-
-
-
-
-
-
-    
 }
